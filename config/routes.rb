9fb10729e9a283cd232b4bac4a17d5cbae3b2bdd
aokazuevirob <102888386+aokazuevirob@users.noreply.github.com>
2022-06-12 06:51:05 +0000

    routes作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'countries/new'
-    get 'countries/index'
-    get 'countries/show'
-    get 'countries/edit'
-  end
-  namespace :admin do
-    get 'reviews/index'
-    get 'reviews/show'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :public do
-    get 'countries/index'
-    get 'countries/show'
-  end
-  namespace :public do
-    get 'reviews/new'
-    get 'reviews/index'
-    get 'reviews/show'
-    get 'reviews/edit'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  get 'countries/index'
-  get 'countries/show'
-  get 'reviews/new'
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/edit'
-  get 'users/show'
-  get 'users/edit'
-  get 'homes/top'
-  get 'homes/about'
-  # 管理者用
+ # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -56,7 +13,27 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  root to: 'public/homes#top'
+  root to: "public/homes#top"
+
+  scope module: :public do
+    get "about" => "homes#about"
+    get "reviews/confirm" => "reviews#confirm"
+    resources :countries, only: [:index, :show]
+    resources :reviews, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+      resources :favorites, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+
+    putch "users/withdraw" => "users#withdraw"
+    get "users/quit" => "users#quit"
+  end
+
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :countries, only: [:new, :index, :show, :edit, :update]
+    resources :reviews, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
