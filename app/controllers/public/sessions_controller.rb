a5e84fe4300835b77a26cd7b4615f8744883dd4d
aokazuevirob <102888386+aokazuevirob@users.noreply.github.com>
2022-06-13 18:28:51 +0000

    会員ログイン作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +19,20 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def user_state
+    # userテーブルからemail情報を確認
+    @user = User.find_by(email: params[:user][:email])
+    # 該当レコードがない場合は処理を終了する
+    return if !@user
+    # passwordが正しいかつ退会ステータスがtrue(退会済)の場合に実行
+      if @user.valid_password?(params[:user][:password]) && @user.is_deleted == true
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_user_registration_path
+      end
+  end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
