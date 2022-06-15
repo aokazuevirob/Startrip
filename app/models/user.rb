2ab6598496b75b64e2eb26a5b06452f2a0fde653
aokazuevirob <102888386+aokazuevirob@users.noreply.github.com>
2022-06-15 16:38:54 +0000

    user/show作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
+      user.last_name = "guestuser"
     end
   end
 
