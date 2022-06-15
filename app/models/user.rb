cd001fadb5881f942d7cdd4b8f603c54644563e7
aokazuevirob <102888386+aokazuevirob@users.noreply.github.com>
2022-06-15 12:17:17 +0000

    devise error修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :nickname, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, {length: {maximum: 50}}
 
   enum gender: { male: 0, female: 1, no_answer: 2}
