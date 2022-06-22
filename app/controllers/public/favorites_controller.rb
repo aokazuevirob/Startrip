7a1b0cdd9fd2625439146505d0fed2adedfc9d26
aokazuevirob <102888386+aokazuevirob@users.noreply.github.com>
2022-06-22 03:59:17 +0000

    いいね非同期実装

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,16 +2,14 @@ class Public::FavoritesController < ApplicationController
 
   def create
     @review = Review.find(params[:review_id])
-    @favorite = current_user.favorites.new(review_id: @review.id)
-    @favorite.save
-    render 'public/replace_btn'
+    favorite = current_user.favorites.new(review_id: @review.id)
+    favorite.save
   end
 
   def destroy
     @review = Review.find(params[:review_id])
-    @favorite = current_user.favorites.find_by(review_id: @review.id)
-    @favorite.destroy
-    render 'public/replace_btn'
+    favorite = current_user.favorites.find_by(review_id: @review.id)
+    favorite.destroy
   end
 
 end
